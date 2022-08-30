894cf18af9ca64d95ccb90b90fd1338b325aac12
zadohu <zadohu22@gmail.com>
2022-08-30 14:02:36 -0500

    deploy

--- memory-card-deploy/src/index.js
+++ memory-card-deploy/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <HashRouter basename='/> 
+        <App />
+    </HashRouter>
+   );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
